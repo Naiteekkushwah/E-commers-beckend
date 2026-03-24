a4caac6bd6fbe4836c0210d89f4f5ee29a8cbb31
Naiteekkushwah <naiteekkushwah@7gmail.com>
2026-03-24 13:55:18 +0530

    second commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
- origin: "http://localhost:5173",
+ origin: "https://e-commers-fernted.vercel.app",
   credentials: true
 }));
 
